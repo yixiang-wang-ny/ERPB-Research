536baa26328208d1b972f9a37cd6047dd7e19538
yixiang-wang-ny <yixiang.wang.ny@gmail.com>
2023-04-02 13:09:19 -0400

    save

--- data_reader.py
+++ data_reader.py
@@ -2,8 +2,14 @@ import pandas as pd
 import os
 from functools import lru_cache
 import datetime as dt
+import dateutil.parser
 
 TS_ERP = 'ERP'
+
+
+def _dt2date(x):
+
+    return dt.date(x.year, x.month, x.day)
 
 
 def load_all_data_sets():
@@ -11,7 +17,8 @@ def load_all_data_sets():
     data_sets = {}
 
     df_erp = pd.read_excel('dataset.xlsx', sheet_name='SHILLER', skiprows=3).iloc[:, 1:4]
-    df_erp['date'] = df_erp['BOM'].apply(lambda x:  dt.datetime.strptime("{:.2f}.01".format(x), "%Y.%m.%d"))
+    df_erp['Date'] = df_erp['BOM'].apply(lambda x:  dateutil.parser.parse("{:.2f}.01".format(x))).apply(_dt2date)
+    df_erp = df_erp[['Date', 'CPI', 'Excess CAPE Yield']].set_index('Date')
     data_sets[TS_ERP] = df_erp
 
 
